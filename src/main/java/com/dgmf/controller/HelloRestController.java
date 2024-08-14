d39d6ffea21aece9d9a82047fe6c39e987c6a104
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-14 15:00:27 +0200

    Adding Two New Rest Endpoints to the HelloRestController

--- src/main/java/com/dgmf/controller/HelloRestController.java
+++ src/main/java/com/dgmf/controller/HelloRestController.java
@@ -10,4 +10,16 @@ public class HelloRestController {
     public String hello() {
         return "Hello World !";
     }
+
+    // Expose "/workout" Endpoint | Return "Run a Hard 5k !"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a Hard 5k !";
+    }
+
+    // Expose "/fortune" Endpoint | Return "Today is your Lucky Day !"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your Lucky Day !";
+    }
 }
