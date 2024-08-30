b6548edaafbf8088176219be2eea5dde662ed7d2
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-30 09:13:55 +0200

    Inject Properties from "application.properties" File

--- src/main/java/com/dgmf/controller/HelloRestController.java
+++ src/main/java/com/dgmf/controller/HelloRestController.java
@@ -1,10 +1,17 @@
 package com.dgmf.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
+    // Inject Properties from "application.properties" File
+    @Value("${teacher.name}")
+    private String teacherName;
+    @Value("${course.name}")
+    private String courseName;
+
     // Expose "/" Endpoint | Return "Hello World"
     @GetMapping("/")
     public String hello() {
@@ -21,5 +28,10 @@ public class HelloRestController {
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Today is your Lucky Day !";
+    }
+
+    @GetMapping("/courseinfo")
+    public String getCourseInfo() {
+        return "Teacher Name: " + teacherName + ", Course Name: " + courseName;
     }
 }
